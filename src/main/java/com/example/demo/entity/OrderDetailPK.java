dedcce6d99336350bf927c642f6e5a7b65294675
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-04 14:59:22 +0800

    06041459

---
+++ src/main/java/com/example/demo/entity/OrderDetailPK.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class OrderDetailPK implements Serializable{
+	private Integer orderId;
+	private Integer productId;
+	@Override
+	public int hashCode() {
+		return Objects.hash(orderId, productId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (!(obj instanceof OrderDetailPK)) return false;
+		OrderDetailPK other = (OrderDetailPK)obj;
+		
+		return orderId == other.orderId && productId == other.productId;
+	}
+	
+	
+	
+
+}
