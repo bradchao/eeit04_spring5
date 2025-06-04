d085501a2325cdb7ad815be42e9c69be002976e3
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-04 09:53:10 +0800

    init

---
+++ src/main/java/com/example/demo/Service/MemberProfileService.java
@@ -0,0 +1,38 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Member;
+import com.example.demo.entity.Profile;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.ProfileRepository;
+
+@Service
+public class MemberProfileService {
+	@Autowired
+	private MemberRepository memberRepository;
+	@Autowired
+	private ProfileRepository profileRepository;
+
+	@Transactional
+	public Member save(Member member, Profile profile) {
+		member.setProfile(profile);
+		return memberRepository.save(member);
+	}
+	
+	@Transactional
+	public Profile setProfileToMember(Long memberId, Profile profile) {
+		Member m = memberRepository.findById(memberId).orElse(null);
+		if (m != null) {
+			m.setProfile(profile);
+			profile.setMember(m);
+			return profileRepository.save(profile);
+		}else {
+			return null;
+		}
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/entity/Profile.java
@@ -0,0 +1,67 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profile")
+public class Profile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "cname")
+	private String cname;
+	
+	@Column(name = "age")
+	private Integer age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	//---------------------
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	
+	
+	
+	
+}
