2e3c30f9389cfc2e7b62ccc08a047b723946134c
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-04 11:25:11 +0800

    06041125

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -6,5 +6,30 @@ import com.example.demo.entity.Member;
 
 public interface MemberRepository 
 	extends JpaRepository<Member, Long>{
+	
+	/*
+	 * 動詞 + By + 屬性名稱
+	 * findByEmail(String email) => Optional<Member>
+	 * findAllByAge(Long age) => List<Member> 
+	 * countByName(String name) => Integer
+	 * deleteByOrderDateAtBefore(Date/LocalDate)
+	 * 
+	 * And/Or
+	 * findByNameAndAge(String name, Integer age)
+	 * findByNameOrAge(String name, Integer age)
+	 * 
+	 * Between/LessThan/GreaterThan/LessThanEqual/GreaterThanEqual
+	 * findByOrderDateBetween(Date/LocalDate, Date/LocalDate)
+	 * 
+	 * Like/NotLike/StartingWith/EndingWith/Containing
+	 * findByNameLike(String keyword)
+	 * 
+	 * IsNull/IsNotNull
+	 * findByLastLoginIsNull(Date/LocalDate)
+	 * 
+	 * OrderBy + 屬性 + Asc/Desc
+	 * findByNameLikeOrderByFirstNameAscAndLastNameDesc(String key)
+	 * 
+	 */
 
 }
