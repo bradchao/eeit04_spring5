0532d28c175b00c35e113ebe518d807fabcbfded
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-06 11:00:49 +0800

    06061100

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,23 @@ public class TestController {
 	public void m2(@PathVariable String token) {
 		String email = JwtTool.parseToken(token);
 		System.out.println(email);
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam String email) {
+		// 驗證基本的帳號密碼
+		
+		return JwtTool.createToken(email);
+	}
+	
+	@GetMapping("/member/m1")
+	public String getMemberData1() {
+		return "很重要的資料1";
+	}
+	
+	@GetMapping("/member/m2")
+	public String getMemberData2() {
+		return "很重要的資料2";
 	}
 	
 	
