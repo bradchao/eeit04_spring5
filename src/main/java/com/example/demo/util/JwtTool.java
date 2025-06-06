d967c377ee4351b930e85df52b5ce8fbe25d44a1
mickeyyo55b <mickeyyo55b@gmail.com>
2025-06-06 10:14:10 +0800

    06061014

---
+++ src/main/java/com/example/demo/util/JwtTool.java
@@ -0,0 +1,27 @@
+package com.example.demo.util;
+
+import java.security.Key;
+import java.security.Signature;
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtTool {
+	private static final long EXP_TIME = 30 * 1000;
+	private static final String SECURET = "BradChao1966DogCat12345677654321";
+	private static final Key key = Keys.hmacShaKeyFor(SECURET.getBytes()); 
+
+	public static String createToken(String email) {
+		String token = Jwts.builder()
+					.setSubject(email)
+					.setIssuedAt(new Date())
+					.setExpiration(new Date(System.currentTimeMillis() + EXP_TIME))
+					.signWith(key, SignatureAlgorithm.HS256)
+					.compact();
+			
+		return token;
+	}
+	
+}
